2db996dd12e8f62e50f155601dc4db8c5c3534e2
FuuKowatty <miedzio.2000@gmail.com>
2023-10-31 16:54:53 +0100

    feat: add architecture image

--- src/main/java/pl/bartoszmech/assignexamseats/student/StudentValidationResult.java
+++
@@ -1,4 +0,0 @@
-package pl.bartoszmech.assignexamseats.student;
-
-record StudentValidationResult() {
-}
---
+++ src/main/java/pl/bartoszmech/assignexamseats/student/StudentsRepository.java
@@ -0,0 +1,2 @@
+package pl.bartoszmech.assignexamseats.student;public interface StudentsRepository {
+}
