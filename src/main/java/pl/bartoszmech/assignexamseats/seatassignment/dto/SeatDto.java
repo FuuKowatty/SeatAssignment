1bf07ad9878ac76d8c9e580d84aae1699668c17b
FuuKowatty <miedzio.2000@gmail.com>
2023-11-04 00:14:24 +0100

    feat: overload constructor without fullName in SeatDto

--- src/main/java/pl/bartoszmech/assignexamseats/seatassignment/dto/SeatDto.java
+++ src/main/java/pl/bartoszmech/assignexamseats/seatassignment/dto/SeatDto.java
@@ -1,9 +1,11 @@
-package pl.bartoszmech.assignexamseats.seatAssignmentGenerator.dto;
+package pl.bartoszmech.assignexamseats.seatassignment.dto;
 
 public record SeatDto(
         Integer column,
         Integer row,
         String fullName
-
 ) {
+    public SeatDto(Integer column, Integer row) {
+        this(column, row, null);
+    }
 }
