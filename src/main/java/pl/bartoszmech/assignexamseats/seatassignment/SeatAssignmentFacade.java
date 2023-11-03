fc7c3a7cd2ebf901b30ee5e3603eec2914ff1126
FuuKowatty <miedzio.2000@gmail.com>
2023-11-04 00:07:39 +0100

    refactor: seat assignment generator v3
    move methods to separate classes

--- src/main/java/pl/bartoszmech/assignexamseats/seatassignment/SeatAssignmentFacade.java
+++ src/main/java/pl/bartoszmech/assignexamseats/seatassignment/SeatAssignmentFacade.java
@@ -1,46 +1,41 @@
-package pl.bartoszmech.assignexamseats.seatAssignmentGenerator;
+package pl.bartoszmech.assignexamseats.seatassignment;
 
 import pl.bartoszmech.assignexamseats.classroom.dto.ClassroomDto;
-import pl.bartoszmech.assignexamseats.seatAssignmentGenerator.dto.SeatAssignmentDto;
-import pl.bartoszmech.assignexamseats.seatAssignmentGenerator.dto.SeatDto;
+import pl.bartoszmech.assignexamseats.seatassignment.dto.SeatAssignmentDto;
+import pl.bartoszmech.assignexamseats.seatassignment.dto.SeatDto;
 import pl.bartoszmech.assignexamseats.student.dto.AllStudentsDto;
 import pl.bartoszmech.assignexamseats.student.dto.StudentDto;
-import java.util.Random;
+import pl.bartoszmech.assignexamseats.validatorResult.ValidatorResultFacade;
 
 
 import java.util.*;
 
 import static java.util.Collections.shuffle;
 
-public class SeatAssignmentGeneratorFacade {
-    public SeatAssignmentDto generateSeatAssignment(ClassroomDto classroomDto, AllStudentsDto presentStudents) {
-        //validate
-        int allSeatsCount = classroomDto.columns() * classroomDto.rows();
-        if(presentStudents.students().size() > allSeatsCount) {}
+public class SeatAssignmentFacade {
+    SeatAssignmentValidator validator;
+    SeatAssignmentGenerator generator;
 
-        //generate
+    public SeatAssignmentFacade(SeatAssignmentValidator validator) {
+        this.validator = validator;
+    }
+
+    public SeatAssignmentDto handleSeatAssignment(ClassroomDto classroomDto, AllStudentsDto presentStudents) {
         int columns = classroomDto.columns();
         int rows = classroomDto.rows();
-        List<SeatDto> seats = new ArrayList<>();
-        for (int row = 1; row <= rows; row++) {
-            for (int column = 1; column <= columns; column++) {
-                seats.add(new SeatDto(column, row, ""));
-            }
-        }
-        shuffle(seats);
+        int allSeatsCount = classroomDto.columns() * classroomDto.rows();
 
-        List<StudentDto> randomizedStudents = presentStudents.students().stream()
-                .sorted(Comparator.comparingDouble(s -> Math.random()))
-                .toList();
+        List<StudentDto> students = presentStudents.students();
+        ValidatorResultFacade validationResult = validator.validate(allSeatsCount, getStudentsCount(students));
 
+        return generator.generate(columns, rows, students);
+    }
 
+    private int getStudentsCount(List<StudentDto> presentStudents) {
+        return presentStudents.size();
+    }
 
-        List<SeatDto> assignedSeats  = new LinkedList<>();
-        for (int i = 0; i < randomizedStudents.size(); i++) {
-            String fullName = randomizedStudents.get(i).firstName() + " " + randomizedStudents.get(i).lastName();
-            assignedSeats.add(new SeatDto(seats.get(i).column(), seats.get(i).row(), fullName));
-        }
-
-        return new SeatAssignmentDto(assignedSeats);
+    private int getClassroomSize(ClassroomDto classroomDto) {
+        return classroomDto.rows() * classroomDto.columns();
     }
 }
