bace7b4fcaac50a459db910f0577581d48e64127
FuuKowatty <miedzio.2000@gmail.com>
2023-11-04 00:06:05 +0100

    feat: handle seat assignment validation

--- src/main/java/pl/bartoszmech/assignexamseats/seatassignment/SeatAssignmentValidationEnum.java
+++ src/main/java/pl/bartoszmech/assignexamseats/seatassignment/SeatAssignmentValidationEnum.java
@@ -1,2 +1,11 @@
-package pl.bartoszmech.assignexamseats.seatAssignmentGenerator;public enum SeatAssignmentValidationEnum {
+package pl.bartoszmech.assignexamseats.seatassignment;
+
+enum SeatAssignmentValidationEnum {
+    TOO_MUCH_STUDENTS("Students count is too big for this classroom");
+
+    final String message;
+    SeatAssignmentValidationEnum(String message) {
+        this.message = message;
+    }
+
 }
--- src/main/java/pl/bartoszmech/assignexamseats/seatassignment/SeatAssignmentValidator.java
+++ src/main/java/pl/bartoszmech/assignexamseats/seatassignment/SeatAssignmentValidator.java
@@ -1,14 +1,28 @@
-package pl.bartoszmech.assignexamseats.seatAssignmentGenerator;
+package pl.bartoszmech.assignexamseats.seatassignment;
 
 import pl.bartoszmech.assignexamseats.validatorResult.ValidatorResultFacade;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static pl.bartoszmech.assignexamseats.validatorResult.ValidatorResultFacade.failure;
 import static pl.bartoszmech.assignexamseats.validatorResult.ValidatorResultFacade.success;
 
 public class SeatAssignmentValidator {
-    public ValidatorResultFacade validate() {
+    List<SeatAssignmentValidationEnum> errors = new LinkedList<>();
+    public ValidatorResultFacade validate(int classroomSize, int studentsCount) {
+        if(classroomSize < studentsCount) {
+            errors.add(SeatAssignmentValidationEnum.TOO_MUCH_STUDENTS);
+        }
 
 
+        return errors.isEmpty() ? success() : failure(concatenateValidationMessage());
+    }
 
-        return success();
+    private String concatenateValidationMessage() {
+        return errors.stream()
+                .map(error -> error.message)
+                .collect(Collectors.joining(", "));
     }
 }
